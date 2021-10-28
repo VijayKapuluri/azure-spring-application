167d9db697e0b856f0cc7426df3e3d887b6e25e4
Vijay <vijaykumar.kapuluri@gmail.com>
2021-10-28 21:53:59 +0530

    first commit

---
+++ src/main/java/com/vijay/azurespringapplication/AzureSpringApplication.java
@@ -0,0 +1,14 @@
+package com.vijay.azurespringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringApplication.class, args);
+		System.out.println("ABCD");
+	}
+
+}
---
+++ src/test/java/com/vijay/azurespringapplication/AzureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.azurespringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
